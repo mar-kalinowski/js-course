021b77662a7fe429517f88b2af7fa782959b6d2a
Marcin Kalinowski <mjk.gdansk@gmail.com>
2022-02-01 22:08:56 +0100

    Math added

--- test.js
+++ test.js
@@ -152,7 +152,7 @@ function getAverage (a,b) {
 
 } */
 
-var myResult = getAverage(7,8) //global variable
+/* var myResult = getAverage(7,8) //global variable
 
 function logResult() {
 
@@ -160,4 +160,16 @@ function logResult() {
 
 }
 
-logResult()
+logResult() */
+
+var a = '5'
+var b = 5
+
+console.log(a + b)
+console.log(typeof (a + b))
+
+console.log(Math.round(7.5))
+console.log(Math.floor(7.5))
+console.log(Math.ceil(7.5))
+console.log(Math.max(7,5,9,15,300))
+console.log(Math.PI)
