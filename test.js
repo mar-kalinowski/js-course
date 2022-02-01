e2cb63066fb7e1c029ea398071d07ebe0733d68e
Marcin Kalinowski <mjk.gdansk@gmail.com>
2022-02-01 15:19:50 +0100

    Break & Continue added

--- test.js
+++ test.js
@@ -111,3 +111,19 @@ for (i = 1; i <= links.length; i++ ) {
 }   
     document.write('all links now looped') */
 
+/* for (i = 0; i< 10; i++) {
+
+    if(i === 5 || i === 3){
+
+        continue
+
+    }
+
+    console.log(i)
+
+    if(i === 7){
+        break
+    }
+}
+
+console.log('I have broken out of the loop') */
