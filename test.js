43635e1d8949fbec8a638d5cd170c039d9a768aa
Marcin Kalinowski <mjk.gdansk@gmail.com>
2022-02-01 15:09:56 +0100

    for loop added

--- test.js
+++ test.js
@@ -95,10 +95,19 @@ if (myAge < 18 || myAge > 30 || myAge ===25) {
     document.write('you can come')
 } */
 
-var age = 5
+/* var age = 5
 
 while (age < 10) {
     console.log('your age is less than 10')
     age++
-}   document.write('you are now over 10')
+}   document.write('you are now over 10') */
+
+/* var links = document.getElementsByTagName('a')
+
+for (i = 1; i <= links.length; i++ ) {
+
+    console.log('this is link number' + i)
+
+}   
+    document.write('all links now looped') */
 
