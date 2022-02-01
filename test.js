f96b0769bec2e2f1ee9526843065294270cdfc52
Marcin Kalinowski <mjk.gdansk@gmail.com>
2022-02-01 13:42:53 +0100

    if statement added

--- test.js
+++ test.js
@@ -1,12 +1,12 @@
-alert("Hello, World");
+/* alert("Hello, World");
 
 var myVariable;
 myVariable =10;
 
 var myVar2 = "hello";
-myVar2 = "hello again"
+myVar2 = "hello again" */
 
-myVar2 = 5;
+/* myVar2 = 5;
 
 var customerAge = 32;
 
@@ -31,5 +31,24 @@ document.write()
 console.log()
 //Booleans
 var iLikeMeat = true
-iLikeMeat = false
+iLikeMeat = false */
 
+//if Statements
+
+var youLikeMeat = true
+
+if (youLikeMeat) {
+    document.write('you like meat')
+} else {
+    document.write('you hate meat')
+}
+
+
+var myNum = 10
+if (myNum == 10) {
+
+    document.write(' myNum is equal to 10')
+
+} else{
+    document.write('myNum is not equal to 10')
+}
