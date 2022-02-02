2159106d585fffc4ec2b604084e3d19b45dd97ba
Marcin Kalinowski <mjk.gdansk@gmail.com>
2022-02-02 22:10:01 +0100

    changing css styles

--- test.js
+++ test.js
@@ -361,6 +361,36 @@ link.classname = 'ninja'
 link.href
 link.style */
 
+/* var title = document.getElementById('page-title')
+
+title.setAttribute('style', 'position: relative')
+
+title.setAttribute('style', 'left: 10px')
+
+title.setAttribute('style', 'position: relative, left: 10px')
+
+title.style.left = '20px'
+
+title.style.top = '10px'
+
+title.style.colot = 'red'
+
+title.style.backgroundColor = 'blue' */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
