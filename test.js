1bc14314a92190e355b122d52d30f9821e438d60
Marcin Kalinowski <mjk.gdansk@gmail.com>
2022-02-02 13:58:05 +0100

    Constructor added

--- test.js
+++ test.js
@@ -273,6 +273,46 @@ myCar2.logDriver()
 console.log(myCar2.maxSpeed)
 console.log(myCar2.drive(50, 3)) */
 
+/* myCar2 = {
+
+    maxSpeed: 70, 
+    driver: 'Jan', 
+    drive: function(speed, time) { 
+        console.log(speed * time)},
+    logDriver: function() {
+        console.log('driver name is ' + this.driver)
+    }
+}
+
+
+myCar2.logDriver()
+console.log(myCar2.maxSpeed)
+console.log(myCar2.drive(50, 3)) */
+
+/* var Car = function(maxSpeed, driver){
+
+    this.maxSpeed = maxSpeed
+    this.driver = driver
+    this.drive = function(speed, time) {
+        console.log(speed * time)
+    }
+    this.logDriver = function() {
+        console.log('driver name is ' + this.driver)
+    }
+
+}
+
+var myCar = new Car(70, 'Gregor')
+var myCar2 = new Car(40, 'Frank')
+var myCar3 = new Car(170, 'Joseph')
+var myCar4 = new Car(78, 'Martin')
+
+myCar.drive(30,5)
+myCar3.logDriver() */
+
+
+
+
 
 
 
